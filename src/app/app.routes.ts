41a5b9ffa7c4ed698869fcf1a6c6aa9bedcfcc3f
Daniel Escobar <danielescobar.987@gmail.com>
2024-06-23 22:36:21 -0600

    Fin del curso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-import { ListComponent } from '@products/pages/list/list.component'
-import { AboutComponent } from '@info/pages/about/about.component';
-import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 import { LayoutComponent } from '@shared/components/layout/layout.component';
 export const routes: Routes = [
     {
@@ -10,16 +7,20 @@ export const routes: Routes = [
         children: [
             {
                 path: '',
-                component: ListComponent
+                loadComponent: () => import('./domains/products/pages/list/list.component')
             },
             {
                 path: 'about',
-                component: AboutComponent
+                loadComponent: () => import('./domains/info/pages/about/about.component')
             },
+            {
+                path: 'product/:id',
+                loadComponent: () => import('./domains/products/pages/product-detail/product-detail.component')
+            }
         ] 
     },
     {
         path: '**',
-        component: NotFoundComponent
+        loadComponent: () => import('./domains/info/pages/not-found/not-found.component')
     }
 ];
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,25 +1,43 @@
-import { Component, inject, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
+
 import { ProductComponent } from '@products/components/product/product.component';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { Product } from '@shared/components/counter/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/components/counter/models/category.model';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent, HeaderComponent],
+  imports: [ProductComponent, HeaderComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export default class ListComponent {
 
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
+  @Input() category_id?: string;
 
   ngOnInit(){
+    this.getCategories();
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    this.getProducts()
+}
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product)
+  }
+
+  private getProducts(){
     this.productService.getProducts()
     .subscribe({
       next: (products) => {
@@ -28,10 +46,17 @@ export class ListComponent {
       error: ()=> {
         console.log('Error fetching products');
       }
-  })
-}
-
-  addToCart(product: Product) {
-    this.cartService.addToCart(product)
+    })
+  }
+  private getCategories(){
+    this.categoryService.getAll()
+    .subscribe({
+      next: (data) => {
+        this.categories.set(data);
+      },
+      error: ()=> {
+        console.log('Error fetching products');
+      }
+    })
   }
 }
